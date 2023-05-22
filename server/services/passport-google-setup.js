2123c80082ad25450642064922674f90254dd429
NikoPanagiotidis <niklas.panagiotidis@gmail.com>
2023-05-22 20:18:26 +0200

    url changes

--- server/services/passport-google-setup.js
+++ server/services/passport-google-setup.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'https://entscape-backend.onrender.com/auth/google/callback',
+      callbackURL: 'https://ent-scape-backend.onrender.com/auth/google/callback',
       scope: ['profile', 'email'],
     },
     async (accessToken, refreshToken, profile, done) => {
